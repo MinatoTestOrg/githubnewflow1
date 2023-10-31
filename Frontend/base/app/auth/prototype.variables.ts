9b38c51717906360c4dc377c9af8c2106f49312f
sukumaran909 <134578580+sukumaran909@users.noreply.github.com>
2023-10-31 18:35:25 +0530

    Commit for the generated/updated files via system

---
+++ Frontend/base/app/auth/prototype.variables.ts
@@ -0,0 +1,5 @@
+export class PrototypeVariables {
+	public static APP_ID = "89ad9329-cd32-42ae-8862-642bc85ff053";
+	public static TENANT_ID = "42f5ccc4-adcf-4729-917c-ab3cbdc31684";
+	public static DESIGN_STUDIO_URL = "https://localhost/";
+}
